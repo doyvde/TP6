2db62260b9d7582a4944d09f965e6b3c42a4fd21
doyvde <95855418+doyvde@users.noreply.github.com>
2022-10-11 14:10:18 +0200

    Initial commit

---
+++ mabiblio.c
@@ -0,0 +1,5 @@
+//
+// Created by denis on 06/10/2022.
+//
+#include "mabiblio.h"
+
---
+++ mabiblio.h
@@ -0,0 +1,9 @@
+//
+// Created by denis on 06/10/2022.
+//
+
+#ifndef TP6_MABIBLIO_H
+#define TP6_MABIBLIO_H
+
+
+#endif //TP6_MABIBLIO_H
